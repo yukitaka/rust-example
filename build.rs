22892bfd712b57200a8d9a724fc30bb6b9f4b2e6
Takayuki Sato <s.yukkiy@gmail.com>
2022-06-08 00:41:48 +0900

    fix: vulnerabilities

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 extern crate prost_build;
 
 fn main() {
-    prost_build::compile_protos(&["src/items.proto", "src/myobj.proto"],
+    prost_build::compile_protos(&["src/myobj.proto"],
                                 &["src/"]).unwrap();
     println!("=============>");
 }
