9a5fcf0c6a8996322899851bd40d8e081bc9bd45
Takayuki Sato <s.yukkiy@gmail.com>
2022-06-10 00:05:42 +0900

    feat: update prost

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-extern crate actix;
-extern crate actix_protobuf;
-extern crate actix_web;
-extern crate bytes;
-extern crate env_logger;
-extern crate prost;
-extern crate prost_derive;
-
 use actix_protobuf::*;
 use actix_web::*;
 
@@ -13,25 +5,23 @@ pub mod my_obj {
     include!(concat!(env!("OUT_DIR"), "/rust_example.rs"));
 }
 
-fn index(msg: ProtoBuf<my_obj::MyObj>) -> Result<HttpResponse> {
+async fn index(msg: ProtoBuf<my_obj::MyObj>) -> Result<HttpResponse> {
     println!("model: {:?}", msg);
     HttpResponse::Ok().protobuf(msg.0) // <- send response
 }
 
-fn main() {
-    ::std::env::set_var("RUST_LOG", "actix_web=info");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    ::std::env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
     env_logger::init();
-    let sys = actix::System::new("prost-example");
 
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
             .service(web::resource("/").route(web::post().to(index)))
     }).bind("0.0.0.0:8080")
-    .unwrap()
-    .shutdown_timeout(1)
-    .start();
-
-    println!("Started http server: 0.0.0.0:8080");
-    let _ = sys.run();
+        .unwrap()
+        .shutdown_timeout(1)
+        .run()
+        .await
 }
